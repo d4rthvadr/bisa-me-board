e833d25deaa9ffae491d0109299f5331ceea001a
darkvadr <glowburn.acc@gmail.com>
2026-08-26 11:14:09 +0000

    feat(ui): add custom 404/500 pages and error handlers

--- board/views.py
+++ board/views.py
@@ -222,3 +222,11 @@ def vote_question(request, board_code, question_id):
     return response
 
 
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def custom_500(request):
+    return render(request, '500.html', status=500)
+
+
